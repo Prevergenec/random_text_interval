b4d494138b99ed5fd7abf0b5f6017c0e0e751745
Prevergenec <prevergenec@ya.ru>
2022-10-31 15:24:38 +0300

    First ready

---
+++ script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+    window.addEventListener('DOMContentLoaded', () => {
+
+        const arrText = ['perfo', 'radio', 'tv', 'closet', 'tractor', 'car', 'wind'],
+        randomText = document.querySelector('#randon_text');
+
+        setInterval(function() {
+            let randId = Math.floor(Math.random() * arrText.length);
+            randomText.classList.toggle('hide');
+            randomText.textContent = arrText[randId];
+        }, 2000);
+
+    });
